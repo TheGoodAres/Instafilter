fa35a7323829376188405fdaf172da9b25da5e67
TheGoodAres <61661335+TheGoodAres@users.noreply.github.com>
2023-04-19 20:08:29 +0100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Robert-Dumitru Oprea on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
