54ae1560ba3a07b33472bf9f3512e74ad6de0ed3
TheGoodAres <61661335+TheGoodAres@users.noreply.github.com>
2023-04-19 22:31:40 +0100

    Initial commit

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -4,18 +4,38 @@
 //
 //  Created by Robert-Dumitru Oprea on 19/04/2023.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
+    @State private var showingImagePicker = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
+            Button("Select Image") {
+                showingImagePicker = true
+            }
+
         }
-        .padding()
+        
+            .padding()
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+            .onChange(of:inputImage) {_ in
+                loadImage()
+            }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else {return}
+        image = Image(uiImage: inputImage)
     }
 }
 
